d53f081b2c8e8a0bf72c760612a4d9e18ed4b190
wolandscat <thomas.beale@openehr.org>
2015-12-14 11:06:26 +0000

    Update UML index generation script to use  {am_release} variable.

--- do_uml_generate-spec-AM.sh
+++ do_uml_generate-spec-AM.sh
@@ -1,2 +1,2 @@
-./uml_generate_win.sh -i latest -o docs/UML computable/UML/openEHR_UML-AM.mdzip
+./uml_generate_win.sh -i {am_latest} -o docs/UML computable/UML/openEHR_UML-AM.mdzip
 ./uml_generate_win.sh -o docs/UML computable/UML/ADL-examples.mdzip
