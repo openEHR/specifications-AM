709487e85f37f976794e10c8aab5dd65f5018e68
wolandscat <thomas.beale@openehr.org>
2016-01-07 18:52:37 +0000

    Update UML generation scripts for MagicDraw 18.2 (uses OSGI containers).

---
+++ do_uml_generate-spec-AM-1.4.sh
@@ -0,0 +1,1 @@
+./uml_generate.sh -i 1.4 -o docs/UML/AOM1.4 computable/UML/openEHR_UML-AM-14.mdzip
--- do_uml_generate-spec-AM.sh
+++ do_uml_generate-spec-AM.sh
@@ -1,2 +1,2 @@
-./uml_generate_win.sh -i {am_release} -o docs/UML computable/UML/openEHR_UML-AM.mdzip
-./uml_generate_win.sh -o docs/UML computable/UML/ADL-examples.mdzip
+./uml_generate.sh -i {am_release} -o docs/UML computable/UML/openEHR_UML-AM.mdzip
+./uml_generate.sh -o docs/UML computable/UML/ADL-examples.mdzip
