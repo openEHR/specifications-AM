97e44a77da23954fc10abbd203a7bd5b6bd3cbe4
wolandscat <thomas.beale@openehr.org>
2017-09-22 18:41:10 +0100

    Clean up generation; fix cross-specification references from AOM specs. See SPECAM-42 .

--- do_uml_generate-spec-AM-1.4.sh
+++ do_uml_generate-spec-AM-1.4.sh
@@ -1,1 +1,4 @@
-./uml_generate.sh -i 1.4 -o docs/UML/AOM1.4 computable/UML/openEHR_UML-AM-14.mdzip
+rm docs/UML/AOM1.4/classes/*.*
+rm docs/UML/AOM1.4/diagrams/*.*
+
+./uml_generate.sh -i 1.4 -r AM -o docs/UML/AOM1.4 computable/UML/openEHR_UML-AM-14.mdzip
--- do_uml_generate-spec-AM.sh
+++ do_uml_generate-spec-AM.sh
@@ -1,2 +1,5 @@
-./uml_generate.sh -i {am_release} -o docs/UML computable/UML/openEHR_UML-AM.mdzip
+rm docs/UML/classes/*.*
+rm docs/UML/diagrams/*.*
+
+./uml_generate.sh -i {am_release} -r AM -o docs/UML computable/UML/openEHR_UML-AM.mdzip
 ./uml_generate.sh -o docs/UML computable/UML/ADL-examples.mdzip
