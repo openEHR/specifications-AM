bfdc84ea36a831d462db25a203ec90551fa3c1bd
wolandscat <thomas.beale@openehr.org>
2017-11-14 10:44:14 +0000

    Add 13606 model publishing stmts to script.

--- do_uml_generate-spec-AM.sh
+++ do_uml_generate-spec-AM.sh
@@ -3,3 +3,9 @@ rm docs/UML/diagrams/*.*
 
 ./uml_generate.sh -i {am_release} -r AM -o docs/UML computable/UML/openEHR_UML-AM.mdzip
 ./uml_generate.sh -o docs/UML computable/UML/ADL-examples.mdzip
+
+# the following generates the same files, but with Cambria font, used for ISO 13606 publication.
+#rm docs/UML_13606/classes/*.*
+#rm docs/UML_13606/diagrams/*.*
+#./uml_generate.sh -i {am_release} -r AM -o docs/UML_13606 computable/UML/openEHR_UML-AM-ISO13606fontchange.mdzip 
+#./uml_generate.sh -o docs/UML_13606 computable/UML/ADL-examples-ISO13606fontchange.mdzip
