caf8f2414112fde574f36b28801e11970be61393
wolandscat <thomas.beale@openehr.org>
2018-12-13 21:44:11 +0000

    Correct UML export problem in ADL examples.

--- do_uml_generate-spec-AM.sh
+++ do_uml_generate-spec-AM.sh
@@ -2,7 +2,7 @@ rm docs/UML/classes/*.*
 rm docs/UML/diagrams/*.*
 
 ./uml_generate.sh -i {am_release} -r AM -o docs/UML computable/UML/openEHR_UML-AM.mdzip
-./uml_generate.sh -o docs/UML computable/UML/ADL-examples.mdzip
+./uml_generate.sh -r AM -o docs/UML computable/UML/ADL-examples.mdzip
 
 # the following generates the same files, but with Cambria font, used for ISO 13606 publication.
 #rm docs/UML_13606/classes/*.*
