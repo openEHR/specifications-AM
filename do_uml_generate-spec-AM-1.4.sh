5f5aba688f76f45c50475b2f617ea9aafbd71d7f
wolandscat <thomas.beale@openehr.org>
2019-05-20 20:14:14 +0100

    Update UML generation scripts.

--- do_uml_generate-spec-AM-1.4.sh
+++ do_uml_generate-spec-AM-1.4.sh
@@ -1,4 +1,4 @@
-rm docs/UML/AOM1.4/classes/*.*
-rm docs/UML/AOM1.4/diagrams/*.*
+rm -f docs/UML/AOM1.4/classes/*.*
+rm -f docs/UML/AOM1.4/diagrams/*.*
 
-./uml_generate.sh -i 1.4 -r AM -o docs/UML/AOM1.4 computable/UML/openEHR_UML-AM-14.mdzip
+../specifications-AA_GLOBAL/bin/uml_generate.sh -i 1.4 -r AM -o docs/UML/AOM1.4 computable/UML/openEHR_UML-AM-14.mdzip
