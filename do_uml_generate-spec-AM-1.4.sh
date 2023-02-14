d06f49621852bbe46c5e0d91ad8e66498bd8ff44
wolandscat <thomas.beale@openehr.org>
2023-02-14 23:22:04 +0000

    Changes for SPECAM-82 - Publishig adjustments.

--- do_uml_generate-spec-AM-1.4.sh
+++
@@ -1,3 +0,0 @@
-rm -f docs/UML/AOM1.4/classes/*.*
-rm -f docs/UML/AOM1.4/diagrams/*.*
-../specifications-AA_GLOBAL/bin/uml_generate.sh -d svg -i {%s_release} -r openehr -q -c AM -o docs/UML/AOM1.4 computable/UML/openEHR_UML-AM-14.mdzip
